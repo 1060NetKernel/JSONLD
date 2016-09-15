71a4733e076aff17f2cc4fcef46246d4c822549d
pjr <pjr@1060.org>
2016-09-15 19:26:00 +0100

    refactored test module

--- module/urn.org.netkernel.json.jsonld/src/org/netkernel/json/jsonld/endpoint/JSONLDCompact.java
+++ module/urn.org.netkernel.json.jsonld/src/org/netkernel/json/jsonld/endpoint/JSONLDCompact.java
@@ -6,19 +6,32 @@
 
 package org.netkernel.json.jsonld.endpoint;
 
+import java.util.HashMap;
+
 import org.netkernel.layer0.meta.impl.SourcedArgumentMetaImpl;
 import org.netkernel.layer0.nkf.INKFRequestContext;
+import org.netkernel.layer0.nkf.INKFResponse;
+import org.netkernel.layer0.representation.IReadableBinaryStreamRepresentation;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
+
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 
 public class JSONLDCompact extends StandardAccessorImpl
 {
 	public JSONLDCompact()
-	{	this.declareThreadSafe();
-		declareArgument(new SourcedArgumentMetaImpl("operand","input JSONLD document",null,new Class[]{String.class}));
+	{	//this.declareThreadSafe();
 		//declareSourceRepresentation(JSONObject.class);
 	}
 	
-	public void onSource(INKFRequestContext context) throws Exception
-	{	context.createResponseFrom("JSONLDCompact TBD");
+	public void onSource(INKFRequestContext aContext) throws Exception
+	{	IReadableBinaryStreamRepresentation opd=aContext.source("arg:operand", IReadableBinaryStreamRepresentation.class);
+		Object jsonObject = JsonUtils.fromInputStream(opd.getInputStream());
+		HashMap context = new HashMap();
+		JsonLdOptions options = new JsonLdOptions();
+		Object compact = JsonLdProcessor.compact(jsonObject, context, options);
+		INKFResponse resp=aContext.createResponseFrom(JsonUtils.toPrettyString(compact));
+		resp.setMimeType("application/ld+json");
 	}
 }
