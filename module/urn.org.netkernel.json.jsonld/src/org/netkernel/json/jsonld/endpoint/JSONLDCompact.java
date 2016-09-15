0c538ff0b4d0c01326939bf730f4867b5836d428
pjr <pjr@1060.org>
2016-09-15 20:26:40 +0100

    Added compact

--- module/urn.org.netkernel.json.jsonld/src/org/netkernel/json/jsonld/endpoint/JSONLDCompact.java
+++ module/urn.org.netkernel.json.jsonld/src/org/netkernel/json/jsonld/endpoint/JSONLDCompact.java
@@ -6,16 +6,24 @@
 
 package org.netkernel.json.jsonld.endpoint;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashMap;
 
+import org.apache.http.client.HttpClient;
 import org.netkernel.layer0.meta.impl.SourcedArgumentMetaImpl;
 import org.netkernel.layer0.nkf.INKFRequestContext;
 import org.netkernel.layer0.nkf.INKFResponse;
 import org.netkernel.layer0.representation.IReadableBinaryStreamRepresentation;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.github.jsonldjava.core.DocumentLoader;
+import com.github.jsonldjava.core.JsonLdError;
 import com.github.jsonldjava.core.JsonLdOptions;
 import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.core.RemoteDocument;
 import com.github.jsonldjava.utils.JsonUtils;
 
 public class JSONLDCompact extends StandardAccessorImpl
@@ -38,10 +46,51 @@ public class JSONLDCompact extends StandardAccessorImpl
 		JsonLdOptions options = new JsonLdOptions();
 		Object compact = JsonLdProcessor.compact(jsonObject, ctxObject, options);
 		
+		
 		//4 Issue response
 		INKFResponse resp=aContext.createResponseFrom(JsonUtils.toPrettyString(compact));
 		
 		//5. Add metadata 
 		resp.setMimeType("application/ld+json");
+	}
+	
+	class MyDL extends DocumentLoader
+	{
+
+		@Override
+		public Object fromURL(URL arg0) throws JsonParseException, IOException
+		{
+			// TODO Auto-generated method stub
+			return super.fromURL(arg0);
+		}
+
+		@Override
+		public HttpClient getHttpClient()
+		{
+			// TODO Auto-generated method stub
+			return super.getHttpClient();
+		}
+
+		@Override
+		public RemoteDocument loadDocument(String arg0) throws JsonLdError
+		{
+			// TODO Auto-generated method stub
+			return super.loadDocument(arg0);
+		}
+
+		@Override
+		public InputStream openStreamFromURL(URL url) throws IOException
+		{
+			// TODO Auto-generated method stub
+			return super.openStreamFromURL(url);
+		}
+
+		@Override
+		public void setHttpClient(HttpClient nextHttpClient)
+		{
+			// TODO Auto-generated method stub
+			super.setHttpClient(nextHttpClient);
+		}
+		
 	}
 }
